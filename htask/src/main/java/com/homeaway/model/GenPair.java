df403f8414328129cdf6f3c1b35248cac602e9e5
carlosd <d3monix@gmail.com>
2016-06-25 00:53:38 -0500

    BTree list implementation

--- htask/src/main/java/com/homeaway/model/GenPair.java
+++ htask/src/main/java/com/homeaway/model/GenPair.java
@@ -1,6 +1,7 @@
 package com.homeaway.model;
 
 public class GenPair< F, S > {
+	int			index;
 	private F	firstElement;
 	private S	secundElement;
 
@@ -30,5 +31,60 @@ public class GenPair< F, S > {
 	public String toString() {
 		return "GenPair [firstElement=" + firstElement + (firstElement != null ? " class=" + firstElement.getClass().getSimpleName() : "") + ", secundElement=" + secundElement
 				+ (secundElement != null ? " class=" + secundElement.getClass().getSimpleName() : "") + "]";
+	}
+	
+	@SuppressWarnings( { "rawtypes", "unchecked" } )
+	public int size() {
+		return size( ((GenPair< GenPair, GenPair >)this).getSecundElement() );
+	}
+	
+
+	@SuppressWarnings( "rawtypes" )
+	public int add( GenPair genPair ) {
+		return add( genPair, this );
+	}
+	
+	@SuppressWarnings( "rawtypes" )
+	public GenPair get( int index ) {
+		return get( index, this );
+	}
+	
+	@SuppressWarnings( { "unchecked", "rawtypes" } )
+	private int size( GenPair< GenPair, GenPair > genPair ) {
+		return (genPair != null ? 1 + size( genPair.getSecundElement() ) : 0);
+	}
+
+	@SuppressWarnings( { "unchecked", "rawtypes" } )
+	private int add( GenPair genPair, GenPair tree ) {
+		if( tree != null ) {
+			if( tree.getSecundElement() != null ) {
+				return 1 + add( genPair, (GenPair)tree.getSecundElement() );
+			} else {
+				tree.setSecundElement( genPair );
+				return 1;
+			}
+		}
+
+		return 0;
+	}
+	
+	@SuppressWarnings( "rawtypes" )
+	private GenPair get( int index, GenPair tree ) {
+		if( tree != null && index > -1 ) {
+			int search = 0;
+
+			GenPair searchedGenPair = (GenPair)tree.getSecundElement();
+		
+			while( searchedGenPair != null && search <= index ) {
+				if( search == index ) {
+					return searchedGenPair;
+				}
+				
+				searchedGenPair = (GenPair)searchedGenPair.getSecundElement();
+				search++;
+			}
+		}
+
+		return null;
 	}
 }
---
+++ htask/src/main/java/com/homeaway/util/GenPairTree.java
@@ -0,0 +1,52 @@
+package com.homeaway.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+
+import com.homeaway.htask.GenPairListTest;
+import com.homeaway.model.GenPair;
+
+public class GenPairTree {
+
+	public static final Log log = LogFactory.getLog( GenPairListTest.class );
+
+	@Test
+	@SuppressWarnings( { "rawtypes", "unchecked" } )
+	public void test() {
+		GenPair gp1 = new GenPair( null, null );
+		GenPair gp2 = new GenPair( null, null );
+		GenPair gp3 = new GenPair( null, null );
+		GenPair gp4 = new GenPair( null, null );
+		GenPair gp5 = new GenPair( null, null );
+
+		GenPair list = new GenPair( null, null );
+		
+		printElementHashCode( "ELEMENT ADDED > " + list.add( gp1 ) + " ", gp1 );
+		printElementHashCode( "ELEMENT ADDED > " + list.add( gp2 ) + " ", gp2 );
+		printElementHashCode( "ELEMENT ADDED > " + list.add( gp3 ) + " ", gp3 );
+		printElementHashCode( "ELEMENT ADDED > " + list.add( gp4 ) + " ", gp4 );
+		printElementHashCode( "ELEMENT ADDED > " + list.add( gp5 ) + " ", gp5 );
+		
+		int size = list.size();
+		
+		log.info( "'List' size: " + size );
+		
+		GenPair element1 = list.get( size - 1 );
+		
+		printElementHashCode( "LAST ELEMENT (list.get(size - 1)) > ", element1 );
+		
+		GenPair gp6 = new GenPair( null, null );
+		
+		log.info( "ELEMENT ADDED > " + list.add( gp6 ) + ", HASHCODE: " + gp6.hashCode() );
+		
+		GenPair element2 = list.get( size );
+		
+		printElementHashCode( "LAST ELEMENT (list.get(size)) >", element2 );
+	}
+	
+	@SuppressWarnings( "rawtypes" )
+	public static final void printElementHashCode( String text, GenPair element ) {
+		log.info( text + (element != null ? "OK" : "null") + ", HASHCODE: " + (element != null ? element.hashCode() : "null") );
+	}
+}
